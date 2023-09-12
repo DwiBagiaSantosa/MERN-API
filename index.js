eaba48d4b5ea58f1d6df33535f0df050f1900cbe
Dwi Bagia Santosa <dwibagiasentosa@gmail.com>
2023-09-12 18:58:35 +0700

    before connect to mongodb

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const app = express();
 
-app.use(() => {
-    console.log('hello server...')
-})
+const authRoutes = require('./src/routes/auth');
+const blogRoutes = require('./src/routes/blog')
 
-app.listen(3000);
+app.use(bodyParser.json())
+app.use(cors())
+
+
+app.use('/v1/auth', authRoutes);
+app.use('/v1/blog', blogRoutes);
+
+app.use((error, req, res, next) => {
+    const status = error.errorStatus || 500;
+    const message = error.message;
+    const data = error.data;
+
+    res.status(status).json({message: message, data: data})
+})
+// app.use('/v1/customer', anotherRoutes);
+
+
+app.listen(4000);
